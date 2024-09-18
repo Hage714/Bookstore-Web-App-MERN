f3618305f3263453ea2491155933b557e166c13e
Hage Woche <66566707+Hage714@users.noreply.github.com>
2024-09-18 15:34:04 +0300

    Changes on deployment

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
-const connectionString = "mongodb://localhost:27017/bookstoredb";
+//const connectionString = "mongodb://localhost:27017/bookstoredb";
+const newConnectionString = "mongodb+srv://hagewoche99:Hage1234@cluster0.o4jhw.mongodb.net/bookstoreApp?retryWrites=true&w=majority&appName=Cluster0"
 
 const connect_database = async () => {
     await mongoose
-        .connect(connectionString)
+        .connect(newConnectionString)
         .then(() => console.log("Database connected successfully"))
-        .catch((error) => console.log(error.message));
+        .catch((error) => console.log(error.message))
 };
 
 module.exports = {
